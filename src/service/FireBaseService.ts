6eb062b6b13ed70ca2059f9d80d51e4799c013b5
DESKTOP-8F9VMMN\hnJin <knsan189@gmail.com>
2022-06-02 16:28:11 +0900

    set Environment variables

--- src/service/FireBaseService.ts
+++ src/service/FireBaseService.ts
@@ -2,14 +2,24 @@ import { initializeApp } from "firebase/app";
 import { collection, doc, getDoc, getDocs, getFirestore, updateDoc } from "firebase/firestore";
 import { IUser } from "../@types/types";
 
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAM0NRUM67CYIUswZfCf9fJ45ki2DsbGR0",
-  authDomain: "starlight-86322.firebaseapp.com",
-  projectId: "starlight-86322",
-  storageBucket: "starlight-86322.appspot.com",
-  messagingSenderId: "55249082470",
-  appId: "1:55249082470:web:f727177da66070e0f675a7",
-  measurementId: "G-GR7NH00KGE",
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
